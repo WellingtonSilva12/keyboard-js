610114fd1ab38c9f0b939091cfd96099db2485d2
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-08-04 16:57:44 -0300

    update javascript in buttons and action delete

---
+++ script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('.btn')
+const inputText = document.querySelector('textarea')
+const btn_capslock = document.querySelector('.capslock')
+const btn_del = document.querySelector('.delete')
+const btn_space = document.querySelector('.space')
+
+let character = []
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    inputText.value += btn.innerText
+    character = inputText.value.split('')
+    console.log(character)
+  })
+})
+
+btn_space.addEventListener('click', () => {
+  character.push(' ')
+  inputText.value = character.join('')
+})
+
+btn_capslock.addEventListener('click', () => {
+  btn_capslock.classList.toggle('active')
+  buttons.forEach(btn => {
+    btn.classList.toggle('upper')
+  })
+})
+
+btn_del.addEventListener('click', () => {
+  character.pop()
+  inputText.value = character.join('')
+})
