527e51b9e9858705acbcf3d59c817034ae82086c
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-08-04 22:05:14 -0300

    update keyboard second character especials

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const buttons = document.querySelectorAll('.btn')
 const inputText = document.querySelector('textarea')
 const btn_capslock = document.querySelector('.capslock')
 const btn_del = document.querySelector('.delete')
+const btn_del1 = document.querySelector('.delete1')
 const btn_space = document.querySelector('.space')
+const toggleChars = document.querySelector('.chars')
+const toggleChars1 = document.querySelector('.chars1')
+const secondKeyboard = document.querySelector('.second-keyboard')
 
 let character = []
 
@@ -29,4 +33,17 @@ btn_capslock.addEventListener('click', () => {
 btn_del.addEventListener('click', () => {
   character.pop()
   inputText.value = character.join('')
+})
+
+btn_del1.addEventListener('click', () => {
+  character.pop()
+  inputText.value = character.join('')
+})
+
+toggleChars.addEventListener('click', () => {
+  secondKeyboard.classList.toggle('hidden-keyboard')
+})
+
+toggleChars1.addEventListener('click', () => {
+  secondKeyboard.classList.toggle('hidden-keyboard')
 })
